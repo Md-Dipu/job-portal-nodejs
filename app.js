b593060f806108a5c9295a73ecd9c16f368fb6e7
Md. Dipu <srabun.tangail@gmail.com>
2023-02-09 15:00:41 +0600

    [add route] user routes

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const cors = require('cors')
 const express = require('express')
 
+const userRoutes = require('./routes/userRoutes')
+
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/user', userRoutes)
 
 app.get('/', (req, res) => {
   res.json({
