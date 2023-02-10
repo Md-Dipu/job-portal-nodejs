e0aa923463e71b1adccb9212bc7eb5428f633149
Md. Dipu <srabun.tangail@gmail.com>
2023-02-10 14:03:14 +0600

    [update] route security

--- routes/jobRouters.js
+++ routes/jobRouters.js
@@ -1,9 +1,16 @@
 const express = require('express')
 
 const jobControllers = require('../controllers/jobControllers')
+const authorization = require('../middleware/authorization')
+const verifyToken = require('../middleware/verifyToken')
 
 const router = express.Router()
 
-router.route('/').post(jobControllers.createNewJobController)
+router.route('/')
+  .post(
+    verifyToken,
+    authorization('hiring-manager'),
+    jobControllers.createNewJobController
+  )
 
 module.exports = router
