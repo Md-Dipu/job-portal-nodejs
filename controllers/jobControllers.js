bcc7388fab15f1067d8256c2e6434d4ae0dc76d6
Md. Dipu <srabun.tangail@gmail.com>
2023-02-11 13:15:04 +0600

    [update] user permission to modify the job data

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -1,6 +1,7 @@
 const {
   createNewJobService,
-  updateJobByIdService
+  updateJobByIdService,
+  getJobById
 } = require('../services/jobServices')
 
 exports.createNewJobController = async (req, res) => {
@@ -24,6 +25,14 @@ exports.createNewJobController = async (req, res) => {
 
 exports.updateJobByIdController = async (req, res) => {
   try {
+    const job = await getJobById(req.params.id)
+    if (job.hiringManager.id.toString() !== req.user._id) {
+      return res.status(403).json({
+        success: false,
+        message: 'User isn\'t authorized to modify this job'
+      })
+    }
+
     const result = await updateJobByIdService(req.params.id, req.body)
     res.status(200).json({
       success: true,
